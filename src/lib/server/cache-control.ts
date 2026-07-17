d333a14cb0798eb1d0c05d421e1bd36460c283a2
jayC02 <jayveerchall17@gmail.com>
2026-07-17 20:44:16 +0100

    Cache fix

--- src/lib/server/cache-control.ts
+++ src/lib/server/cache-control.ts
@@ -1,6 +1,6 @@
 export const publicStatic = 'public, max-age=31536000, immutable';
-export const publicPageShort = 'public, max-age=0, s-maxage=30, stale-while-revalidate=60';
-export const publicPageMedium = 'public, max-age=0, s-maxage=300, stale-while-revalidate=600';
+export const publicPageShort = 'public, max-age=0, must-revalidate';
+export const publicPageMedium = 'public, max-age=0, must-revalidate';
 export const publicApiShort = 'public, max-age=0, s-maxage=30, stale-while-revalidate=60';
 export const privateNoStore = 'private, no-store, max-age=0, must-revalidate';
 export const privateApiNoStore = 'private, no-store, max-age=0, must-revalidate';
